13ec5d202c0914873a47a4f8c3890d9d661fe932
EgorKopitsa <kopitsa.egor.2020@gmail.com>
2020-11-11 17:18:57 +0300

    Settings

--- shop/settings.py
+++ shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!wpn=77(kiur=)e)^j&f2r3r#e^xp#
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hidden-woodland-73459.herokuapp.com']
 
 
 # Application definition
