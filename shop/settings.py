9cbedc4a572a9e386faa51cc023fa072133f86a6
EgorKopitsa <kopitsa.egor.2020@gmail.com>
2020-11-11 18:03:43 +0300

    Debug

--- shop/settings.py
+++ shop/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!wpn=77(kiur=)e)^j&f2r3r#e^xp#um+9epq_5v7)@67ogzl2')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = ['hidden-woodland-73459.herokuapp.com']
+DEBUG = False
+ALLOWED_HOSTS = ['hidden-woodland-73459.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
